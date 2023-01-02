50ca4b6103ddc4f0a64954721d3f9f33ab9d9c49
Huy <duchuy58cntt@gmail.com>
2023-01-02 22:20:00 +0700

    set sp, images, favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 function App() {
   return (
@@ -6,9 +7,26 @@ function App() {
         <a href="/">Trầm Nhi's Store</a>
       </header>
       <main>
-        list products
-      </main>
-    </div>
+        <h1>Sản phẩm nổi bật</h1>
+        <div className="sanpham">
+          {
+            data.sanpham.map( sp => (
+              <div className="sp" key={ sp.slug }>
+                <a href={ `/sanpham/${ sp.slug }` }>
+                  <img src={ sp.anh } alt={ sp.tensp } />
+                </a>
+                <div className="sp-thongtin">
+                  <a href={ `/sanpham/${ sp.slug }` }>
+                    <p>{ sp.tensp }</p>
+                  </a>
+                  <p><strong>{ sp.gia } VNĐ</strong></p>
+                  <button>Thêm vào giỏ</button>
+                </div>
+              </div> ) )
+          }
+        </div>
+      </main >
+    </div >
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+    sanpham: [
+        {
+            tensp: 'Áo trẻ em 1',
+            slug: 'ao-tre-em-1',
+            loaisp: 'ao',
+            anh: '/images/a1.jpg',
+            gia: 80000,
+            soluong: 20,
+            ncc: 'Supreme',
+            danhgia: 5,
+            sldanhgia: 10,
+            mota: 'áo nam dành cho trẻ em chất lượng cao',
+        },
+        {
+            tensp: 'Quần trẻ em 1',
+            slug: 'quan-tre-em-1',
+            loaisp: 'quan',
+            anh: '/images/q1.jpg',
+            gia: 80000,
+            soluong: 20,
+            ncc: 'Supreme',
+            danhgia: 4.5,
+            sldanhgia: 10,
+            mota: 'quần nam dành cho trẻ em chất lượng cao',
+        },
+        {
+            tensp: 'Mũ trẻ em 1',
+            slug: 'mu-tre-em-1',
+            loaisp: 'mu',
+            anh: '/images/m1.jpg',
+            gia: 50000,
+            soluong: 20,
+            ncc: 'Supreme',
+            danhgia: 5,
+            sldanhgia: 10,
+            mota: 'mũ nam dành cho trẻ em chất lượng cao',
+        },
+        {
+            tensp: 'Váy trẻ em 1',
+            slug: 'vay-tre-em-1',
+            loaisp: 'vay',
+            anh: '/images/v1.jpg',
+            gia: 50000,
+            soluong: 20,
+            ncc: 'Supreme',
+            danhgia: 5,
+            sldanhgia: 10,
+            mota: 'váy dành cho trẻ em chất lượng cao',
+        }
+    ],
+};
+export default data;
