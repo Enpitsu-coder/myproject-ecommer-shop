2f4b2fe6feead243bf9973f35f720fd918388516
Huy <duchuy58cntt@gmail.com>
2023-01-03 21:25:40 +0700

    add bootstrap, footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
 import SanPhamScreen from './screens/SanPhamScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <Link to="/">Trầm Nhi's Store</Link>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="black" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Trầm Nhi's Store</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/sanpham/:slug" element={ <SanPhamScreen /> } />
-            <Route path="/" element={ <HomeScreen /> } />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/sanpham/:slug" element={ <SanPhamScreen /> } />
+              <Route path="/" element={ <HomeScreen /> } />
+            </Routes>
+          </Container>
         </main >
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div >
     </BrowserRouter>
   );
